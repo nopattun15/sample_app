bb55451240a5e7d765416954352a721936251d60
takumi.ito <nopattun15@gmail.com>
2021-03-06 00:11:05 +0900

    chapter13 "Operate microposts & Upload images "

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
 
-  def hello
-    render html: "Hello, World!"
-  end
+  private 
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
+    end
 end
